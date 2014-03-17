b0e8f6e746825cfaa1cb3120ce361109b2d7271e
Peter Zijlstra <peterz@infradead.org>
2014-03-17 18:06:10 +0100

    arch: Mass conversion of smp_mb__*()
    Mostly scripted conversion of the smp_mb__* barriers.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/n/tip-55dhyhocezdw1dg7u19hmh1u@git.kernel.org
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-arch@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Conflicts:
    	drivers/usb/dwc3/core.c
    	net/netfilter/nf_conntrack_core.c

--- arch/sparc/include/asm/barrier_64.h
+++ arch/sparc/include/asm/barrier_64.h
@@ -37,18 +37,10 @@ do {	__asm__ __volatile__("ba,pt	%%xcc, 1f\n\t" \
 #define rmb()	__asm__ __volatile__("":::"memory")
 #define wmb()	__asm__ __volatile__("":::"memory")
 
-<<<<<<< HEAD
-#define read_barrier_depends()		do { } while(0)
-=======
 #define dma_rmb()	rmb()
 #define dma_wmb()	wmb()
 
-<<<<<<< HEAD
->>>>>>> 1077fa3... arch: Add lightweight memory barriers dma_rmb() and dma_wmb()
-#define set_mb(__var, __value) \
-=======
 #define smp_store_mb(__var, __value) \
->>>>>>> b92b8b3... locking/arch: Rename set_mb() to smp_store_mb()
 	do { WRITE_ONCE(__var, __value); membar_safe("#StoreLoad"); } while(0)
 
 #ifdef CONFIG_SMP
