2a6db297759a9f6bb6f94dc7c5cbdfd5485c3703
alucard24 <dmbaoh2@gmail.com>
2015-06-01 15:58:12 +0200

    build scripts: Linaro 4.9.1 -> 4.9.3  5.1-compass commit ebbb2d2a85a929d7fb5857bd2dfa087e6d0a014e 1 parent c201e39 @AntaresOne AntaresOne authored 4 days ago

--- build_kernel_cr_4.9.3.sh
+++ build_kernel_cr_4.9.3.sh
@@ -14,8 +14,12 @@ export PACKAGEDIR=$KERNELDIR/READY-JB
 #Enable FIPS mode
 export USE_SEC_FIPS_MODE=true
 export ARCH=arm
-export CROSS_COMPILE=$PARENT_DIR/../cr-arm-eabi-4.9.1/bin/arm-eabi-
-export KERNEL_CONFIG=alucard_defconfig;
+export CROSS_COMPILE=$PARENT_DIR/../cr-arm-eabi-4.9.3/bin/arm-eabi-
+if [! "$1" == "" ]; then
+    export KERNEL_CONFIG="$1";
+else
+    export KERNEL_CONFIG=alucard_defconfig;
+fi
 
 chmod -R 777 /tmp;
 
