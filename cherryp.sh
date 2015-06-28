a66542343b621b860432f5e94ed0d192f900ab8e
alucard24 <dmbaoh2@gmail.com>
2015-06-28 15:47:15 +0200

    Removed useless file

--- cherryp.sh
+++
@@ -1,4 +0,0 @@
-git rev-list --reverse --topo-order d17b3a329053b2a5dad7e8fdbd7760969b490b94^..b23c14a7f9a642881801fac124dc87462fd17ccf | while read rev 
-do 
-  git cherry-pick $rev || break 
-done 
