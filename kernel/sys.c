21fade0aec4ebd2abd4c777d807ee063cb6876c0
alucard24 <dmbaoh2@gmail.com>
2015-07-25 12:17:38 +0200

    Code style!

--- kernel/sys.c
+++ kernel/sys.c
@@ -2233,6 +2233,9 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		case PR_GET_TID_ADDRESS:
 			error = prctl_get_tid_address(me, (int __user **)arg2);
 			break;
+		case PR_SET_VMA:
+			error = prctl_set_vma(arg2, arg3, arg4, arg5);
+			break;
 		case PR_SET_TIMERSLACK_PID:
 			if (task_pid_vnr(current) != (pid_t)arg3 &&
 					!capable(CAP_SYS_NICE))
@@ -2252,8 +2255,6 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 				tsk->timer_slack_ns = arg2;
 			put_task_struct(tsk);
 			error = 0;
-		case PR_SET_VMA:
-			error = prctl_set_vma(arg2, arg3, arg4, arg5);
 			break;
 		case PR_SET_CHILD_SUBREAPER:
 			me->signal->is_child_subreaper = !!arg2;
